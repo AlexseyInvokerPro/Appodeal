73d2883b8c18ceb35bf3d0ee3c44af422d1cb80f
Avdeychik <a.avdeychik@gmail.com>
2022-05-10 17:24:26 +0300

    Add Pods

---
+++ AppodealTest/AppodealTest/Native/NativeView.swift
@@ -0,0 +1,63 @@
+//
+//  NativeView.swift
+//  AppodealTest
+//
+//  Created by Алексей Авдейчик on 10.05.22.
+//
+
+import UIKit
+import Appodeal
+
+
+final class NativeView: UIView {
+    
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var callToAction: UILabel!
+
+    @IBOutlet weak var descr: UILabel!
+    @IBOutlet weak var icon: UIImageView!
+    @IBOutlet weak var mediaContainer: UIView!
+    @IBOutlet weak var adChoices: UIView!
+    
+    override func draw(_ rect: CGRect) {
+        self.layer.cornerRadius = 16
+        self.layer.masksToBounds = true
+        
+        self.icon.layer.cornerRadius = 8.0
+        self.icon.layer.masksToBounds = true
+
+        self.callToAction.layer.cornerRadius = 8.0
+        self.callToAction.layer.masksToBounds = true
+    }
+}
+
+extension NativeView : APDNativeAdView {
+    func titleLabel() -> UILabel {
+        return title
+    }
+    
+    func callToActionLabel() -> UILabel {
+        return callToAction
+    }
+    
+    func descriptionLabel() -> UILabel {
+        return descr
+    }
+    
+    func iconView() -> UIImageView {
+        return icon
+    }
+    
+    func mediaContainerView() -> UIView {
+        return mediaContainer
+    }
+    
+    func adChoicesView() -> UIView {
+        return adChoices
+    }
+    
+    static func nib() -> UINib {
+        return UINib.init(nibName: "Native", bundle: Bundle.main)
+    }
+}
+
