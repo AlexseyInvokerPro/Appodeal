e95f0cc1535ec542bb5af022616dc75c0b6dd092
Avdeychik <a.avdeychik@gmail.com>
2022-05-10 17:24:35 +0300

    fix

--- AppodealTest/AppodealTest/Supporting files/AppDelegate.swift
+++ AppodealTest/AppodealTest/Supporting files/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Appodeal
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,8 +14,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+        let types: AppodealAdType = [.banner, .rewardedVideo, .interstitial, .nativeAd]
+        Appodeal.initialize(withApiKey: "3135b2cde254cd761b6dfc261a59c2c3", types: types)
+       return true
     }
 
     // MARK: UISceneSession Lifecycle
@@ -25,12 +27,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {}
 }
 
--- AppodealTest/AppodealTest/ViewControllers/ViewController.swift
+++ AppodealTest/AppodealTest/ViewControllers/ViewController.swift
@@ -6,14 +6,124 @@
 //
 
 import UIKit
+import Appodeal
 
 class ViewController: UIViewController {
-
+    
+    var bannerCount = 0
+    
+    @IBOutlet weak var bannerTop: UIButton!
+    @IBOutlet weak var interstitials: UIButton!
+    @IBOutlet weak var rewardedVideo: UIButton!
+    @IBOutlet weak var native: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        bannerConfigure()
+        interstitialsConfigure()
+        rewardedVideoConfigure()
+        
+        view.backgroundColor = .green
+        
     }
-
-
+    
+    private func bannerConfigure() {
+        
+        Appodeal.cacheAd(.banner)
+        Appodeal.isReadyForShow(with: .bannerTop)
+        Appodeal.setBannerDelegate(self)
+        Appodeal.isInitalized(for: .banner)
+        
+    }
+    
+    private func interstitialsConfigure() {
+        
+        Appodeal.cacheAd(.interstitial)
+        Appodeal.isReadyForShow(with: .interstitial)
+        Appodeal.setInterstitialDelegate(self)
+        Appodeal.isInitalized(for: .interstitial)
+        Appodeal.predictedEcpm(for: .interstitial)
+    }
+    
+    private func rewardedVideoConfigure() {
+        
+        Appodeal.cacheAd(.rewardedVideo)
+        if Appodeal.isReadyForShow(with: .rewardedVideo) {
+//            rewardedVideo.isEnabled = false
+        }
+  
+        Appodeal.setRewardedVideoDelegate(self)
+        Appodeal.isInitalized(for: .rewardedVideo)
+        Appodeal.predictedEcpm(for: .rewardedVideo)
+    }
+    
+    @IBAction func bannerTopButtonPressed(_ sender: UIButton) {
+        
+        
+        Appodeal.showAd(.bannerTop, rootViewController: self)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            Appodeal.hideBanner()
+            self.bannerCount += 1
+        }
+        
+        if bannerCount == 4 {
+            bannerTop.isHidden = true
+        }
+    }
+    
+    @IBAction func interstitialsButtonPressed(_ sender: UIButton) {
+        
+        Appodeal.showAd(.interstitial, rootViewController: self)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 60) {
+            self.interstitials.isEnabled = true
+        }
+        
+        interstitials.isEnabled = false
+        
+    }
+    
+    @IBAction func rewardedVideoButtonPressed(_ sender: UIButton) {
+        Appodeal.showAd(.rewardedVideo, rootViewController: self)
+    }
+    
+    @IBAction func nativeButtonPressed(_ sender: UIButton) {
+        
+        let vc = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "Native")
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
 }
+
+extension ViewController: AppodealBannerDelegate {
+    
+    func bannerDidLoadAdIsPrecache(_ precache: Bool) {}
+    func bannerDidShow() {}
+    func bannerDidFailToLoadAd() {}
+    func bannerDidClick() {}
+    func bannerDidExpired() {}
+}
+
+extension ViewController: AppodealInterstitialDelegate {
+
+    func interstitialDidLoadAdIsPrecache(_ precache: Bool) {}
+    func interstitialDidFailToLoadAd() {}
+    func interstitialDidFailToPresent() {}
+    func interstitialWillPresent() {}
+    func interstitialDidDismiss() {}
+    func interstitialDidClick() {}
+    func interstitialDidExpired(){}
+}
+
+extension ViewController: AppodealRewardedVideoDelegate {
+
+    func rewardedVideoDidLoadAdIsPrecache(_ precache: Bool) {}
+    func rewardedVideoDidFailToLoadAd() {}
+    func rewardedVideoDidFailToPresentWithError(_ error: Error) {}
+    func rewardedVideoDidPresent() {}
+    func rewardedVideoWillDismissAndWasFullyWatched(_ wasFullyWatched: Bool) {}
+    func rewardedVideoDidFinish(_ rewardAmount: Float, name rewardName: String?) {}
+    func rewardedVideoDidClick() {}
+    func rewardedVideoDidExpired() {}
+}
+
 
